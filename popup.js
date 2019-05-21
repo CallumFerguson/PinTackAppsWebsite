68932a82244a46f44b7fe2cfcd0a157840c79aef
Callum Ferguson <Calxferg@gmail.com>
2019-05-21 12:13:31 -0500

    removed php and extras

--- popup.js
+++ popup.js
@@ -4,7 +4,5 @@ function openPopup() {
 
 function closePopup() {
     $("#popup").css("display", "none");
-    
-    //from https://stackoverflow.com/questions/15164942/stop-embedded-youtube-iframe
     $("#popup > iframe")[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
 }
