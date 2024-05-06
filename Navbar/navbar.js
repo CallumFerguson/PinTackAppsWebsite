a8a173cd16c80b8311a45245e3127951853f2fea
Callum Ferguson <Calxferg@gmail.com>
2024-05-06 15:06:02 -0700

    clean up project

--- Navbar/navbar.js
+++ Navbar/navbar.js
@@ -1,16 +1,25 @@
 var page = parseInt(document.currentScript.getAttribute("page"));
 
+function prefixPath(path) {
+    const isHomePage = page === 0;
+    if (isHomePage) {
+        return "./" + path;
+    } else {
+        return "../" + path;
+    }
+}
+
 var navbarHtml = `
 <ul id="navbar" class="clearfix">
-    <li><a href="/">Home</a></li>
-    <li><a href="/overkilldrill">Overkill Drill</a></li>
-    <li><a href="/piperacer">Pipe Racer</a></li>
+    <li><a href="${page === 0 ? "" : prefixPath("")}">Home</a></li>
+    <li><a href="${page === 1 ? "" : prefixPath("overkilldrill")}">Overkill Drill</a></li>
+    <li><a href="${page === 2 ? "" : prefixPath("piperacer")}">Pipe Racer</a></li>
 </ul>
-`
+`;
 
 document.write(navbarHtml);
 
 $(function() {
-    if(page != -1)
+    if(page !== -1)
         $("#navbar").find("a")[page].classList.add("active");   
 });
--- final/index.js
+++
@@ -1,3 +0,0 @@
-$(() => {
-    window.location.href = "http://webglgame.com.s3-website.us-east-2.amazonaws.com/game/"
-})
